cb9495031d64542e03a2d0bb9105daf7b3b1f2de
opashaj5 <opashaj5@yahoo.com>
2022-04-26 21:27:31 -0500

    update index.js and create new files in src

--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 
-
 db.on('connected', () => {
     console.log(`Connected to ${db.name} at ${db.host}:${db.port}`)
 });
